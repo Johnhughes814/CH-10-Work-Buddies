b9c9946e15d4e89c04736cbfc48bd8bf59c5cb5e
Johnhughes814 <8hughes.john@gmail.com>
2022-03-15 11:53:58 -0500

    HTML renderer

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const Intern = require ("./lib/Intern")
 const path = require ("path") 
 const fs = require ("fs")
 const inquirer = require("inquirer")
-const { async } = require("rxjs")
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
 
 buildTeam()
 
-const team = []
+const team = [];
 
 async function buildTeam() {
     console.log("Good morning! Let's bring some new hires on board!")
